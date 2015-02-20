962f83418fda7b86c3b36a4237a528f0b6443725
henry van dusen <hgv2102@columbia.edu>
2015-02-20 13:49:08 -0500

    okay okay okay

---
+++ app.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var express = require('express');
+var logger = require('morgan');
+var ejs = require("ejs")
+var socketio = require('socket.io');
+var _ = require('underscore');
+
+
+// create the express app
+var app = express();
+app.use(express.static(__dirname + '/public'));
+// connect the Morgan logging middleware to our app
+app.use( logger('dev') );
+
+
+
+// start a server listening on port 1234
+var server = app.listen( 1234 );
+var io = socketio.listen( server );
+var pathList = [];
+var clients = [];
+var gearCount = 0;
+app.set('view engine','ejs')
+
+    io.sockets.on('connection', function (socket) {
+
+      clients.push(socket.client.id)
+      console.dir(clients)
+      socket.emit('project', { paths: pathList });
+
+
+
+    socket.on('newPath', function (data) {
+      console.log('added a new path. now at '+gearCount+' paths')
+      pathList[gearCount]= data.newPath
+      gearCount++
+      io.sockets.emit('update',{paths:pathList})
+    });
+
+
+
+    socket.on('giveup',function(data){
+        io.sockets.emit('erase')
+        gearCount = 0;
+        pathList = []
+    });
+    socket.on('disconnect',function(){
+      clients.pop(socket.client.id)
+
+    });
+});
+app.get('/', function (request, response) {
+    response.render('mainView');
+});
+
+app.get('/:room', function (request, response) {
+    console.dir(request.params)
+    var room = io.of(request.params.room);
+    //response.locals.room = request.params
+    response.render( 'room',{room: request.params.room});
+});
---
+++ public/script.js
@@ -0,0 +1,142 @@
+var path;
+var socket = io.of( '/'+$('#roomname')[0].classList[0]);
+var gears = [];
+
+
+$(document).bind('mousemove', function(e){
+	$('#message').css('color','black');
+    $('#message').css({
+        left: e.pageX + 5,
+        top: e.pageY - 20
+    });
+});
+
+room = $('#roomname')[0].classList[0])
+
+$('#giveup').click(function(){
+	socket.emit('giveup')
+
+});
+
+function onMouseDown(event){
+	$('#message').css('visibility','hidden');
+	path = new Path();
+	path.strokeColor = prettyRaCo();
+	path.strokeWidth = 6;
+	//socket.emit('mousedown',[event.point.x,event.point.y])
+}
+
+function onMouseDrag(event){
+	path.add(event.point);
+	//socket.emit('mouseup',event.point)
+points.push(event.point);
+}
+
+function onMouseUp(event){
+	if(path.segments.length>0)
+		{
+		gear(path.segments);
+		pathPoints = [];
+		$.each(path.segments,function(i,j){
+			pathPoints[i] = [j.point.x,j.point.y]
+		})
+		//console.dir(pathPoints)
+	//	socket.emit('mouseup')
+		path.smooth();
+		socket.emit('newPath',{newPath: pathPoints})
+	}
+}
+
+function gear(gearPath){
+	var circle = new Path.Circle(new Point(-100,-100), new Size(20,20));
+	circle.fillColor = prettyRaCo();
+	gears.push({
+		loc: circle,
+		points: gearPath,
+		len: gearPath.length
+	});
+	socket.emit('newgear',{points:gearPath});
+	//console.dir(gears.);
+
+}
+
+function onFrame(event){
+if(event.count<15000)
+	for(var i = 0;i<gears.length;i++){
+		gears[i].loc.position = gears[i].points[ event.count%gears[i].len].point;
+	}
+}
+
+//default message to put in the thing
+
+	//var circle = new Path.Circle(point,new Size(60,60));
+
+
+
+socket.on('update', function (data) {
+	console.log('update!!')
+	paths = data.paths
+	project.clear();
+	$.each(paths,function(i,j){
+		p = new Path()
+		$.each(j,function(m,n){
+			p.add(new Point(n))
+		});
+		p.strokeColor = prettyRaCo();
+		p.strokeWidth = 6;
+		p.smooth()
+		gear(p.segments)
+	});
+
+});
+
+// TIP: .connect with no args does auto-discovery
+socket.on('project', function (data) {
+	if(data.paths.length == 0)
+	{
+		console.log('nothing here yet');//'sending this as project');
+	}
+	else
+	{
+		paths = data.paths//.importJSON(data.project);
+		console.log('incoming gears list')
+		console.dir(paths)
+		//project.clear();
+
+		$.each(paths,function(i,j){
+			p = new Path()
+			$.each(j,function(m,n){
+				p.add(new Point(n))
+			});
+			console.dir(p)
+			p.strokeColor = prettyRaCo();
+			p.strokeWidth = 6;
+			p.smooth()
+			gear(p.segments)
+		});
+	}
+
+
+	});
+
+	socket.on('newPath', function (data) {
+		livepath = new Path();
+		livepath.strokeColor = prettyRaCo();
+		livepath.strokeWidth = 6;
+	});
+
+	socket.on('move', function (data) {
+		console.dir(data.point)
+		livepath.add(new Point(data.point))
+	});
+
+	socket.on('endPath', function (data) {
+		gear(livepath.segments)
+		livepath.smooth()
+	});
+
+
+	socket.on('erase', function (data) {
+		//pathList = []
+		project.clear();
+	});
